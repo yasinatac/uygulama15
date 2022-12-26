1328b66d0e3aec9e3d0bb8e5528f0ba41df947be
yasinatac <yasinatacc@icloud.com>
2022-12-26 14:00:35 +0300

    uygulama15

---
+++ app/src/androidTest/java/Bilgiler.java
@@ -0,0 +1,14 @@
+public class Bilgiler {
+    private String adiSoyadi;
+    private String telefonNo;
+    private String eMail;
+    public String Bilgiler(String adiSoyadi, String telefonNo, String eMail){
+        this.adiSoyadi = adiSoyadi;
+        this.eMail = eMail;
+        this.telefonNo = telefonNo;
+        {
+            return adiSoyadi;
+        }
+    }
+    }
+
